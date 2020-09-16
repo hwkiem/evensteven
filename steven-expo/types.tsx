4cad55a5d060714a1d6ec38e142f030bd86392dc
hwkiem <hwkiem@gmail.com>
2020-09-15 22:01:57 -0700

    initial expo commit

---
+++ steven-expo/types.tsx
@@ -0,0 +1,20 @@
+import { ParamListBase } from "@react-navigation/native";
+
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type TabOneParamList = {
+  TabOneScreen: undefined;
+};
+
+export type TabTwoParamList = {
+  TabTwoScreen: undefined;
+};
+
+export type MaterialTopTabParamList = {
+  Groups: undefined;
+  Personal: undefined;
+  Pantry: undefined;
+};
